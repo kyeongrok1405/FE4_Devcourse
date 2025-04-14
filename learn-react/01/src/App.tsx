a101d0f0d223b47e8f531e184a0c9fff4890ee8f
kyeongrok <kyeong7214@gmail.com>
2025-04-14 16:48:30 +0900

    첫 커밋

---
+++ learn-react/01/src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  const uname = "kyeongsik";
+  return (
+  <div>
+    <div>Hello, World! {uname}</div>
+    <div>Hello, World! {10 + 20}</div>
+    <input type="text" maxLength={3} />
+    <br />
+  </div>
+);
+}
